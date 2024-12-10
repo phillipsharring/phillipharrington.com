3b8fd28086c4b772ba9aa84b90ea6e91d5aec990
Phillip Harrington <pharrington@abre.com>
2024-12-10 09:50:27 -0500

    - Some content and layout updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,34 @@
-document.getElementById("hamburger-menu").addEventListener("click", () => {
+document.getElementById('hamburger-menu').addEventListener('click', () => {
   toggleOpen();
 });
 
-const body = document.getElementById("body");
-const mainNav = document.getElementById("main-nav");
+const wrapper = document.getElementById('wrapper');
+const mainNav = document.getElementById('main-nav');
 
-const navLinks = mainNav.querySelectorAll("a");
+const navLinks = mainNav.querySelectorAll('a');
 
-const bar1 = document.getElementById("bar-1");
-const bar2 = document.getElementById("bar-2");
-const bar3 = document.getElementById("bar-3");
+const bar1 = document.getElementById('bar-1');
+const bar2 = document.getElementById('bar-2');
+const bar3 = document.getElementById('bar-3');
 
 // closed, open
 const bars = [
   {
-    element: body,
-    classLists: ["overflow-auto", "overflow-hidden"],
+    element: wrapper,
+    classLists: ['overflow-auto', 'overflow-hidden'],
   },
   {
     element: mainNav,
-    classLists: ["hidden", "flex flex-col inset-x-0 top-0"],
+    classLists: ['hidden', 'flex flex-col inset-x-0 top-0'],
   },
   {
     element: bar1,
-    classLists: ["-translate-y-0.5", "rotate-45 translate-y-1"],
+    classLists: ['-translate-y-0.5', 'rotate-45 translate-y-1'],
   },
-  { element: bar2, classLists: ["opacity-100", "opacity-0"] },
+  { element: bar2, classLists: ['opacity-100', 'opacity-0'] },
   {
     element: bar3,
-    classLists: ["translate-y-0.5", "-rotate-45 -translate-y-1"],
+    classLists: ['translate-y-0.5', '-rotate-45 -translate-y-1'],
   },
 ];
 
@@ -37,7 +37,7 @@ let isOpen = false;
 const toggleOpen = () => {
   bars.forEach(({ element, classLists }) => {
     classLists.forEach((classList) => {
-      const classes = classList.split(" ");
+      const classes = classList.split(' ');
       classes.forEach((className) => {
         element.classList.toggle(className);
       });
@@ -48,7 +48,7 @@ const toggleOpen = () => {
 
   navLinks.forEach((navLink) => {
     navLink.addEventListener(
-      "click",
+      'click',
       () => {
         // e
         // e.preventDefault();
