e835be155e04f42867bd2f232a92f22d47925264
Phillip Harrington <pharrington@abre.com>
2024-12-10 11:26:11 -0500

    - Fixes scroll thing - Fixes colors on mobile menu - Fixes image on did you run this

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@ document.getElementById('hamburger-menu').addEventListener('click', () => {
   toggleOpen();
 });
 
-const wrapper = document.getElementById('wrapper');
+const body = document.getElementById('body');
+// const wrapper = document.getElementById('wrapper');
 const mainNav = document.getElementById('main-nav');
 
 const navLinks = mainNav.querySelectorAll('a');
@@ -14,7 +15,7 @@ const bar3 = document.getElementById('bar-3');
 // closed, open
 const bars = [
   {
-    element: wrapper,
+    element: body,
     classLists: ['overflow-auto', 'overflow-hidden'],
   },
   {
@@ -23,12 +24,21 @@ const bars = [
   },
   {
     element: bar1,
-    classLists: ['-translate-y-0.5', 'rotate-45 translate-y-1'],
+    classLists: [
+      '-translate-y-0.5 bg-stone-300',
+      'bg-stone-800 rotate-45 translate-y-1',
+    ],
   },
-  { element: bar2, classLists: ['opacity-100', 'opacity-0'] },
+  {
+    element: bar2,
+    classLists: ['opacity-100 bg-stone-300', 'bg-stone-800 opacity-0'],
+  },
   {
     element: bar3,
-    classLists: ['translate-y-0.5', '-rotate-45 -translate-y-1'],
+    classLists: [
+      'translate-y-0.5 bg-stone-300',
+      'bg-stone-800 -rotate-45 -translate-y-1',
+    ],
   },
 ];
 
